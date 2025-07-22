61250024ad309954cbf7e6ccc4617c1038b02a66
Rohana Upadhyay <workrohanu23@gmail.com>
2025-07-22 10:12:37 +0000

    final fix

--- user-data.sh
+++ user-data.sh
@@ -13,7 +13,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 echo ">>> Pulling Strapi Docker image"
-sudo docker pull brohan9/strapi-app=${docker_tag}
+sudo docker pull brohan9/strapi-app:${docker_tag}
 
 echo ">>> Removing old Strapi container if exists"
 sudo docker rm -f strapi || true
@@ -24,7 +24,7 @@ sudo docker run -d \
   -p 1337:1337 \
   --restart unless-stopped \
   -e APP_KEYS="myKeyA,myKeyB" \
-  brohan9/strapi-app=${docker_tag}
+  brohan9/strapi-app:${docker_tag}
 
 echo ">>> Done setting up Strapi"
 
